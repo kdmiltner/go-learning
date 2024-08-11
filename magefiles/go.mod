4699a64bcb1292cef4b90636ba2816c73aecf434
KayDee Miltner <kaydeemiltner@gmail.com>
2024-08-11 12:24:07 -0400

    Add magefiles

---
+++ magefiles/go.mod
@@ -0,0 +1,3 @@
+module magefiles
+
+go 1.22.4
---
+++ magefiles/magefile.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"io/fs"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	// goModPath represents the go.mod path `/go.mod`.
+	goModPath = "/go.mod"
+	//goModRoot represents the go.mod path at the root directory.
+	goModRoot = "go.mod"
+	// rootPath sets the root directory path.
+	rootPath = "."
+	// mainAppName represents the name of the app at the root level.
+	mainAppName = "go-learning"
+)
+
+// Sets global and env variables that can be accessed in multiple functions.
+var (
+	appRecords []AppRecord
+)
+
+// AppRecord holds the information about an app record. An AppRecord is created for any directory that
+// contains a go.mod file.
+type AppRecord struct {
+	AppPath string
+	AppName string
+}
+
+func Run() {
+	fmt.Println(compileAppRec("magefiles/mage/go.mod"))
+}
+
+func HelloCFA() {
+	fmt.Println("Hello, Chick-fil-A!")
+}
+
+func CompileApps() error {
+	err := compileRecords()
+	fmt.Println(appRecords)
+
+	return err
+}
+
+// compileRecords walks the root directory to compile a list of `[]AppRecord`.
+func compileRecords() error {
+	err := filepath.WalkDir(rootPath, func(path string, dir fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Process paths and handle case where path can equal the root go.mod.
+		if strings.Contains(path, goModPath) || path == goModRoot {
+			record := compileAppRec(path)
+			appRecords = append(appRecords, record)
+		}
+
+		return nil
+	})
+
+	return err
+}
+
+// compileAppRec compiles all the app information into an `AppRecord{}`.
+func compileAppRec(path string) AppRecord {
+	record := AppRecord{}
+
+	// Handle case where the root directory has a go.mod.
+	if path == goModRoot {
+		record.AppPath = filepath.Base(filepath.Dir(path))
+		record.AppName = mainAppName
+
+		return record
+	}
+
+	path, _ = strings.CutSuffix(path, goModPath)
+	record.AppPath = path
+	record.AppName = strings.Replace(path, "/", "-", -1)
+
+	return record
+}
