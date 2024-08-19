fea51e23cd60d423db2af1d9a293496403f700ce
KayDee Miltner <kaydeemiltner@gmail.com>
2024-08-19 09:27:36 -0400

    separating hello world example

---
+++ magefiles/magefile_hello_world.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// HelloCFA is a mage target that prints out "Hello, Chick-fil-A!".
+// Run with the command: `mage helloCFA`.
+func HelloCFA() {
+	fmt.Println("Hello, Chick-fil-A!")
+}
