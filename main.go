eb540ac1bf8e80c9750082c3d1b3ed0891da18e2
KayDee Miltner <kaydeemiltner@gmail.com>
2026-04-01 13:15:21 -0400

    Update chi

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
 func main() {
+	routes()
 }
 
-//func routes() {
-//	r := chi.NewRouter()
-//	r.Use(middleware.Logger)
-//	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-//		w.Write([]byte("Hello World"))
-//	})
-//	http.ListenAndServe(":8080", r)
-//}
+func routes() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+	http.ListenAndServe(":8080", r)
+}
